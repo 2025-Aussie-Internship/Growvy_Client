9f7d97e443f05e655e94a69e776171eb32900fb9
cuzurmyhabit <s2472@e-mirim.hs.kr>
2026-01-07 20:36:08 +0900

    style: Apply Pretendard font

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false, 
-      title: 'Sign Up',
+      title: 'Growvy', // 앱 이름 수정
       theme: ThemeData(
+        // [중요] pubspec.yaml에 정의한 폰트 패밀리 이름과 똑같이 써야 합니다.
+        fontFamily: 'Pretendard', 
+        
         scaffoldBackgroundColor: Colors.white,
         useMaterial3: true,
       ),
